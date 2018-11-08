e2bdf0d0a74d8c313e2d8c94d2e3ecbd267a45d8
ducx <adu003@163.com>
2018-11-08 16:03:47 +0800

    调整项目结构

--- lmt-code-generator/src/main/java/com/lmt/code/example/mapper/ModelMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lmt.code.example.mapper;
-
-import com.lmt.code.example.basic.mapper.BasicModelMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ModelMapper extends BasicModelMapper {
-
-}
